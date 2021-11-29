5686b15b1842f2dc21305d293efa2bd7f7f893aa
Siddhesh Kothadi <realsiddheshko@gmail.com>
2021-11-29 11:48:29 +0530

    adds twit for statuses/update endpoint

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const axios = require("axios").default;
 let qs = require("qs");
+const Twit = require("twit");
 
 const lastIdProcessedSchema = new Schema({
   sinceId: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
-const LastIdProcessed = mongoose.model("lastIdProcessed", lastIdProcessedSchema);
+const LastIdProcessed = mongoose.model(
+  "lastIdProcessed",
+  lastIdProcessedSchema
+);
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
@@ -26,20 +28,60 @@ const USERNAME = process.env.USERNAME;
 const BEARER_TOKEN = process.env.BEARER_TOKEN;
 const RAPID_API_HOST = process.env.RAPID_API_HOST;
 const RAPID_API_KEY = process.env.RAPID_API_KEY;
+const API_KEY = process.env.API_KEY;
+const API_KEY_SECRET = process.env.API_KEY_SECRET;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+const T = new Twit({
+  consumer_key: API_KEY,
+  consumer_secret: API_KEY_SECRET,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET,
+});
+
+function replyToTweet(tweetId, text, username, lastProcessedId) {
+  const finalTweetText = `@${username} ${text}`;
+  T.post(
+    "statuses/update",
+    {
+      status: finalTweetText,
+      in_reply_to_status_id: tweetId,
+    },
+    function (err, data, response) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        // Update the last processed id
+        lastProcessedId.sinceId = tweetId;
+        console.log("updating last processed id to", tweetId);
+        lastProcessedId
+          .save()
+          .then(() => {
+            console.log("updated last processed id");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+  );
+}
 
 async function translateText(text) {
   var options = {
-    method: 'POST',
-    url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+    method: "POST",
+    url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
     headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'accept-encoding': 'application/gzip',
-      'x-rapidapi-host': RAPID_API_HOST,
-      'x-rapidapi-key': RAPID_API_KEY,
+      "content-type": "application/x-www-form-urlencoded",
+      "accept-encoding": "application/gzip",
+      "x-rapidapi-host": RAPID_API_HOST,
+      "x-rapidapi-key": RAPID_API_KEY,
     },
-    data: qs.stringify({q: text, target: 'mar'})
+    data: qs.stringify({ q: text, target: "mar" }),
   };
-  console.log('options');
+  console.log("options");
   console.log(options);
 
   const response = await axios(options);
@@ -49,17 +91,14 @@ async function translateText(text) {
   return response.data.data.translations[0].translatedText;
 }
 
-async function replyToTweet(tweetId, text) {
-}
-
 async function fetchMentions() {
   try {
     const lastProcessedId = await LastIdProcessed.findOne({});
     const sinceId = lastProcessedId?.sinceId;
-    console.log('last processed id is', sinceId);
+    console.log("last processed id is", sinceId);
 
-    let tweetMentionURL = `https://api.twitter.com/2/users/${ID}/mentions?expansions=referenced_tweets.id`;
-    if(sinceId && sinceId.length > 0) {
+    let tweetMentionURL = `https://api.twitter.com/2/users/${ID}/mentions?expansions=referenced_tweets.id,referenced_tweets.id.author_id`;
+    if (sinceId && sinceId.length > 0) {
       tweetMentionURL += `&since_id=${sinceId}`;
     }
 
@@ -67,73 +106,79 @@ async function fetchMentions() {
 
     const response = await axios.get(tweetMentionURL, {
       headers: {
-        Authorization: `Bearer ${BEARER_TOKEN}`
-      }
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+      },
     });
 
-    const {data, includes} = response.data;
+    const { data, includes } = response.data;
 
-    const tweetsToReplyTo = data.map(tweet => {
-      if(tweet.referenced_tweets && tweet.referenced_tweets.length > 0) {
-        // It's a reply to a tweet above
-        // So translate the tweet id to the tweet it's a reply to
-        console.log('reply')
-        const tweetId = tweet.referenced_tweets[0].id;
-        const tweetText = includes.tweets.find(tweet => tweet.id === tweetId).text.replace(/\n/g, ' ');
+    if (data) {
+      const tweetsToReplyTo = data
+        .map((tweet) => {
+          if (tweet.referenced_tweets && tweet.referenced_tweets.length > 0) {
+            // It's a reply to a tweet above
+            // So translate the tweet id to the tweet it's a reply to
+            console.log("reply");
+            const tweetId = tweet.referenced_tweets[0].id;
+            const tweetText = includes.tweets
+              .find((tweet) => tweet.id === tweetId)
+              .text.replace(/\n/g, " ");
+            const username = includes.users.find(
+              (user) => user.id === tweet.author_id
+            ).username;
 
-        return {
-          id: tweet.id,
-          text: tweetText
-        };
-      } else {
-        // It's a tweet itself which is to be translated
-        console.log('normal tweet')
-        return {
-          id: tweet.id, 
-          text: tweet.text.replace(/\n/g, ' ').replace(`@${USERNAME}`, '')
-        };
+            return {
+              id: tweet.id,
+              text: tweetText,
+              username: username,
+            };
+          } else {
+            // It's a tweet itself which is to be translated
+            console.log("normal tweet");
+
+            const username = includes.users.find(
+              (user) => user.id === tweet.author_id
+            ).username;
+
+            return {
+              id: tweet.id,
+              text: tweet.text.replace(/\n/g, " ").replace(`@${USERNAME}`, ""),
+              username: username,
+            };
+          }
+        })
+        .sort((a, b) => {
+          return a.id - b.id;
+        });
+
+      console.log("following tweets are to be replied to");
+      console.log(tweetsToReplyTo);
+
+      // Translate all tweets and reply them one by one
+      for (const tweet of tweetsToReplyTo) {
+        const translatedText = await translateText(tweet.text.trim());
+        console.log("translated text is", translatedText);
+        replyToTweet(tweet.id, translatedText, tweet.username, lastProcessedId);
       }
-    });
-
-    console.log('following tweets are to be replied to');
-    console.log(tweetsToReplyTo);
-
-    // Translate all tweets and reply them one by one
-    for(const tweet of tweetsToReplyTo) {
-      const translatedText = await translateText(tweet.text);
-      console.log('translated text is', translatedText);
-      await replyToTweet(tweet.id, translatedText);
-
-      // Update the last processed id
-      lastProcessedId.sinceId = tweet.id;
-      console.log('updating last processed id to', tweet.id);
-      await lastProcessedId.save();
+    } else {
+      console.log("no tweets to reply to");
     }
 
     setTimeout(fetchMentions, 30000);
-
-    // axios.get(tweetMentionURL, {
-    //   headers: {
-    //     Authorization: `Bearer ${BEARER_TOKEN}`
-    //   }
-    // }).then(response => {
-    //   const {data, includes} = response.data;
-    //   console.log(data);
-    //   console.log(includes);
-    // }).catch(error => console.log(error));
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
 
-mongoose.connect(ATLAS_URI, { 
+mongoose
+  .connect(ATLAS_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Database connection successful");
-    // fetchMentions();
-    translateText('hello world!');
-}).catch(error => console.log(error));
+    fetchMentions();
+  })
+  .catch((error) => console.log(error));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
